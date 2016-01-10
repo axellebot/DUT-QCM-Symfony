a2d3aa6ba00c9e97c95a52d9b98afca682004220
Ilias Redissi <ilias.redissi@gmail.com>
2016-01-10 17:37:35 +0100

    Login page

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new AppBundle\AppBundle(),
+            new IUT\QCMBundle\IUTQCMBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
