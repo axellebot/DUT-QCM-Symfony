001230602e223fe788aea2d4429f57774000a8c5
Ilias Redissi <ilias.redissi@gmail.com>
2016-01-14 18:26:15 +0100

    Rajout des routes

--- src/IUT/QCMBundle/Controller/DefaultController.php
+++ src/IUT/QCMBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace IUT\QCMBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
